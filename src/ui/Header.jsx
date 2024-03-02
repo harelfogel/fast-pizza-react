ddff5c45c33acb05f1b78379dbb57810672e7011
harelfogel <fogell06@gmail.com>
2024-03-02 23:16:15 +0200

    adding folder staructure

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <header>
+            <Link to='/' >Fast React Pizza Company</Link>
+
+            <p>Jonas</p>
+        </header>
+    )
+}
+
+export default Header
